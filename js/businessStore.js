51f52e2600138262424d78ff2d113f14cf81c43b
ShabdVeyyakula <47516044+ShabdVeyyakula@users.noreply.github.com>
2021-03-27 20:03:44 -0700

    needs linking

--- js/businessStore.js
+++ js/businessStore.js
@@ -6,7 +6,7 @@ function loadAuctionItems() {
             var data = value.data();
 
             var auctionItem = `
-                <div class="auction-item" style="border-style: solid; border-color: #141414; border-width: 2px; height: 300px; width: 220px; margin-top: 5%; margin-left: 5%;">
+                <div class="auction-item" style="border-style: solid; border-color: #141414; border-width: 2px; height: 300px; width: 220px; margin-top: 5%; margin-left: 5%;" data-toggle="modal" data-target="#exampleModal">
                     <div class="col">
                         <div class="row">
                             <div style="border-style: solid; border-left-color: #83c264; width: 100px; height: 40px; margin-top: 10%; margin-left: 6%; background-color:#191919 ; color: #191919;">
@@ -108,6 +108,29 @@ function loadAuctionItems() {
 
 
                     </div>
+
+
+                    
+                <!-- Modal -->
+                <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        ...
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                    </div>
+                </div>
+                </div>
             `
 
 
